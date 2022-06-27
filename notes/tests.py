5337b63bd49c07057abcd310f9a301815b08e1e3
Sean Tolbert <stolbert4150@gmail.com>
2022-06-27 15:31:48 -0400

    integrates testing for notes and notes_api

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from notes.models import Note, Category
 
-# Create your tests here.
+
+class Test_Create_Note(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name='django')
+        testuser1 = User.objects.create_user(
+            username='test_user1', password='123456789'
+        )
+        test_post = Note.objects.create(
+            category_id=1,
+            title='Note title',
+            excerpt='excerpt here',
+            content="note content",
+            slug='post-title',
+            author_id=1,
+            status='published'
+        )
+
+    def test_notes_content(self):
+        note = Note.noteobjects.get(id=1)
+        cat = Category.objects.get(id=1)
+        author = f'{note.author}'
+        excerpt = f'{note.excerpt}'
+        title = f'{note.title}'
+        content = f'{note.content}'
+        status = f'{note.status}'
+        self.assertEqual(author, 'test_user1')
+        self.assertEqual(title, 'Post Title')
+        self.assertEqual(content, 'Post Content')
+        self.assertEqual(status, 'published')
+        self.assertEqual(str(note), "Post Title")
+        self.assertEqual(str(cat), "django")
--- notes_api/tests.py
+++ notes_api/tests.py
@@ -1,3 +1,15 @@
+from audioop import reverse
+from django.urls import reverse
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from notes.models import Note, Category
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class NoteTests(APITestCase):
+    def test_view_notes(self):
+        url = reverse('notes_api:listcreate')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
