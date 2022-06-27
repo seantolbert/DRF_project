646664214b770bd5b068549878ec1d703bf87e6d
Sean Tolbert <stolbert4150@gmail.com>
2022-06-27 12:45:54 -0400

    inits repo

---
+++ notes_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from .views import NoteList, NoteDetail
+
+app_name = 'notes_api'
+
+urlpatterns = [
+    # path('<int:pk>/', NoteDetail.as_view(), name='detailcreate'),
+    # path('', NoteList.as_view(), name="listcreate")
+]
