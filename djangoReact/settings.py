d81d3db11bf99c37ee9b0bd3d3b05679b39b3d37
Sean Tolbert <stolbert4150@gmail.com>
2022-06-28 11:40:31 -0400

    completes connecdtion

--- djangoReact/settings.py
+++ djangoReact/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'notes',
     'notes_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,6 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.isAuthenticatedOrReadOnly'
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
